060e5a7328ad40138d20d9b05b4ace181680d935
Lenar Fattahov <lenarfattahov@cloud.upwork.com>
2017-09-24 18:24:11 +0300

    asfasa

--- gruzovoz-zakaz/main.bundle.js
+++ gruzovoz-zakaz/main.bundle.js
@@ -21,7 +21,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".root {\r\n  width: 100%;\r\n  max-width: 640px;\r\n  min-width: 320px;\r\n}\r\n", ""]);
+exports.push([module.i, ".root {\r\n  width: 100%;\r\n  /*max-width: 480px;*/\r\n  min-width: 320px;\r\n}\r\n\r\n.left-column {\r\n  width: 320px;\r\n  z-index: 1;\r\n  position: relative;\r\n}\r\n\r\n.right-column {\r\n  position: fixed;\r\n  top: 0;\r\n  padding-left: 330px;\r\n}\r\n\r\n@media screen and (max-width: 1024px) {\r\n  .right-column {\r\n    display: none;\r\n  }\r\n  .left-column {\r\n    width: 100%;\r\n    max-width: 800px;\r\n    margin: auto;\r\n  }\r\n}\r\n", ""]);
 
 // exports
 
@@ -34,7 +34,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"root\">\n  <app-search-form></app-search-form>\n  <app-points></app-points>\n</div>\n"
+module.exports = "<div class=\"root\">\n  <div class=\"left-column\">\n    <app-search-form></app-search-form>\n    <app-points></app-points>\n  </div>\n  <div class=\"right-column\">\n    <app-map></app-map>\n  </div>\n</div>\n"
 
 /***/ }),
 
@@ -102,12 +102,14 @@ var _a, _b;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__angular_platform_browser_animations__ = __webpack_require__("../../../platform-browser/@angular/platform-browser/animations.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__points_points_component__ = __webpack_require__("../../../../../src/app/points/points.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__events_service__ = __webpack_require__("../../../../../src/app/events.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__map_map_component__ = __webpack_require__("../../../../../src/app/map/map.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -129,7 +131,8 @@ AppModule = __decorate([
         declarations: [
             __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
             __WEBPACK_IMPORTED_MODULE_7__searchForm_searchForm_component__["a" /* SearchFormComponent */],
-            __WEBPACK_IMPORTED_MODULE_9__points_points_component__["a" /* PointsComponent */]
+            __WEBPACK_IMPORTED_MODULE_9__points_points_component__["a" /* PointsComponent */],
+            __WEBPACK_IMPORTED_MODULE_11__map_map_component__["a" /* MapComponent */]
         ],
         imports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
@@ -297,6 +300,128 @@ EventsService = __decorate([
 
 /***/ }),
 
+/***/ "../../../../../src/app/map/map.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/map/map.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"map\">\n  <div id=\"map\" style=\"width:400px; height:300px\"></div>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/map/map.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MapComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_add_operator_map__ = __webpack_require__("../../../../rxjs/add/operator/map.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_add_operator_map___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_rxjs_add_operator_map__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_observable_of__ = __webpack_require__("../../../../rxjs/add/observable/of.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs_add_observable_of___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs_add_observable_of__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__events_service__ = __webpack_require__("../../../../../src/app/events.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+
+var MapComponent = (function () {
+    function MapComponent(eventsService) {
+        var _this = this;
+        this.eventsService = eventsService;
+        this.ymaps = window['ymaps'];
+        this.initMap();
+        eventsService.on('endLoadPoints', function (res, form) {
+            _this.drawPoints(res.data.items);
+        });
+        eventsService.on('startLoadPoints', function () {
+            _this.removeAllPoints();
+        });
+        eventsService.on('openPoint', function (point, searchForm) {
+            _this.drawRoute([searchForm.lat, searchForm.lng], [point.lat, point.lng]);
+        });
+    }
+    MapComponent.prototype.initMap = function () {
+        var _this = this;
+        document.addEventListener('DOMContentLoaded', function (event) {
+            document.getElementById('map').style.width = window.innerWidth - 320 + 'px';
+            document.getElementById('map').style.height = window.innerHeight + 'px';
+            _this.ymaps.ready(function () {
+                _this.map = new _this.ymaps.Map('map', {
+                    center: [55.76, 37.64],
+                    zoom: 8
+                });
+            });
+        });
+    };
+    MapComponent.prototype.drawPoints = function (items) {
+        var _this = this;
+        items.forEach(function (item) {
+            var newPoint = new _this.ymaps.GeoObject({
+                geometry: {
+                    type: 'Point',
+                    coordinates: [item.lat, item.lng]
+                },
+                properties: {
+                    hintContent: item.address
+                }
+            });
+            _this.map.geoObjects.add(newPoint);
+        });
+    };
+    MapComponent.prototype.removeAllPoints = function () {
+        this.map.geoObjects.removeAll();
+    };
+    MapComponent.prototype.drawRoute = function (startPoint, endPoint) {
+        var _this = this;
+        this.map.geoObjects.remove(this.currentRoute);
+        this.ymaps.route([startPoint, endPoint]).then(function (route) {
+            _this.currentRoute = route;
+            _this.map.geoObjects.add(route);
+        }, function (error) {
+            alert('Возникла ошибка: ' + error.message);
+        });
+    };
+    return MapComponent;
+}());
+MapComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-map',
+        template: __webpack_require__("../../../../../src/app/map/map.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/map/map.component.css")]
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_3__events_service__["a" /* EventsService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_3__events_service__["a" /* EventsService */]) === "function" && _a || Object])
+], MapComponent);
+
+var _a;
+//# sourceMappingURL=map.component.js.map
+
+/***/ }),
+
 /***/ "../../../../../src/app/points/points.component.css":
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -305,7 +430,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, ".points-list {\r\n  position: relative;\r\n  max-width: 640px;\r\n  width: 100%;\r\n}\r\n\r\n.point {\r\n  display: inline-block;\r\n  min-width: 320px;\r\n  width: 100%;\r\n  //border: 2px solid #9b4dca;\r\n  border-radius: 5px;\r\n  padding: 10px 10px;\r\n  margin-bottom: 10px;\r\n  background: #f4f5f6;\r\n  cursor: pointer;\r\n}\r\n\r\ninput[type=\"text\"], input[type=\"tel\"] {\r\n  background: #ffffff;\r\n}\r\n\r\n.order-form {\r\n  max-height: 0;\r\n  transition: max-height 0.5s ease;\r\n  overflow: hidden;\r\n}\r\n\r\n.opened .order-form {\r\n  max-height: 300px;\r\n}\r\n", ""]);
+exports.push([module.i, ".points-list {\r\n  position: relative;\r\n  width: 100%;\r\n}\r\n\r\n.point {\r\n  display: inline-block;\r\n  font-size: 14px;\r\n  min-width: 320px;\r\n  width: 100%;\r\n  padding: 20px 10px;\r\n  margin-bottom: 10px;\r\n  background: #e7ffea;\r\n  cursor: pointer;\r\n  transition: background 0.3s ease;\r\n}\r\n\r\n.point.opened {\r\n  background: #b7debc;\r\n  color: black;\r\n}\r\n\r\ninput[type=\"text\"], input[type=\"tel\"] {\r\n  background: #ffffff;\r\n}\r\n\r\n.order-form {\r\n  max-height: 0;\r\n  transition: max-height 0.5s ease;\r\n  overflow: hidden;\r\n}\r\n\r\nform {\r\n  margin-bottom: 0;\r\n}\r\n\r\n.opened .order-form {\r\n  max-height: 300px;\r\n}\r\n\r\ni {\r\n  width: 5%;\r\n  margin-right: 2%;\r\n  text-align: center;\r\n}\r\n\r\ntd:last-child {\r\n  text-align: right;\r\n}\r\n\r\ntable {\r\n  width: 86%;\r\n  margin-left: 7%;\r\n  margin-top: 10px;\r\n}\r\n\r\n.table-wrapper {\r\n  position: relative;\r\n}\r\n\r\n.table-icon {\r\n  position: absolute;\r\n  top: 6px;\r\n}\r\n\r\ntd {\r\n  border-color: #606c76;\r\n}\r\n\r\ntr:last-child td {\r\n  border-bottom: none;\r\n}\r\n\r\ntd:first-child {\r\n  width: 50%;\r\n  border-right: 1px solid #606c76;\r\n}\r\n\r\np {\r\n  margin-bottom: 1.5rem;\r\n}\r\n\r\n", ""]);
 
 // exports
 
@@ -318,7 +443,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/points/points.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"sk-circle\" *ngIf=\"pointsAreLoading\">\n  <div class=\"sk-circle1 sk-child\"></div>\n  <div class=\"sk-circle2 sk-child\"></div>\n  <div class=\"sk-circle3 sk-child\"></div>\n  <div class=\"sk-circle4 sk-child\"></div>\n  <div class=\"sk-circle5 sk-child\"></div>\n  <div class=\"sk-circle6 sk-child\"></div>\n  <div class=\"sk-circle7 sk-child\"></div>\n  <div class=\"sk-circle8 sk-child\"></div>\n  <div class=\"sk-circle9 sk-child\"></div>\n  <div class=\"sk-circle10 sk-child\"></div>\n  <div class=\"sk-circle11 sk-child\"></div>\n  <div class=\"sk-circle12 sk-child\"></div>\n</div>\n<div class=\"points-list\" *ngIf=\"!pointsAreLoading\">\n  <div class=\"point\" *ngFor=\"let point of points; let i = index\"\n    (click)=\"onPointClick(i)\"\n    [ngClass]=\"{'opened': openedPoint==i}\">\n    <h5 class=\"title\">Погрузка №{{point.id}}</h5>\n    <div>{{point.name}}</div>\n    <div>Адрес доставки: {{point.address}}</div>\n    <div>Расстояние: {{point.distance}}км</div>\n    <div>Цена товара: {{point.price}}</div>\n    <div>Стоимость доставки: {{point.delivery}}</div>\n    <div class=\"order-form\">\n      <form>\n        <label for=\"name\">Имя</label>\n        <input id=\"name\" type=\"text\" name=\"name\" [(ngModel)]=\"name\">\n        <label for=\"phone\">Телефон</label>\n        <input id=\"phone\" type=\"tel\" name=\"phone\" [(ngModel)]=\"phone\">\n        <input type=\"submit\" value=\"Заказать\" (click)=\"onOrderClick(point)\">\n      </form>\n    </div>\n  </div>\n</div>\n"
+module.exports = "<div class=\"sk-circle\" *ngIf=\"pointsAreLoading\">\n  <div class=\"sk-circle1 sk-child\"></div>\n  <div class=\"sk-circle2 sk-child\"></div>\n  <div class=\"sk-circle3 sk-child\"></div>\n  <div class=\"sk-circle4 sk-child\"></div>\n  <div class=\"sk-circle5 sk-child\"></div>\n  <div class=\"sk-circle6 sk-child\"></div>\n  <div class=\"sk-circle7 sk-child\"></div>\n  <div class=\"sk-circle8 sk-child\"></div>\n  <div class=\"sk-circle9 sk-child\"></div>\n  <div class=\"sk-circle10 sk-child\"></div>\n  <div class=\"sk-circle11 sk-child\"></div>\n  <div class=\"sk-circle12 sk-child\"></div>\n</div>\n<div class=\"points-list\" *ngIf=\"!pointsAreLoading\">\n  <h6 class=\"count\" *ngIf=\"points\">Найдено {{points.length}} предложений</h6>\n  <div class=\"point\" *ngFor=\"let point of points; let i = index\"\n    (click)=\"onPointClick(i)\"\n    [ngClass]=\"{'opened': openedPoint==i}\">\n    <h5 class=\"title\">\n      <i class=\"fa fa-check-circle\" aria-hidden=\"true\"></i>Погрузка №{{point.id}}</h5>\n    <p><strong><i class=\"fa fa-check-square\" aria-hidden=\"true\"></i>{{point.name}}</strong></p>\n    <p><i class=\"fa fa-address-book-o\" aria-hidden=\"true\"></i>Адрес доставки: {{point.address}}</p>\n    <p><i class=\"fa fa-arrows-v\" aria-hidden=\"true\"></i>Расстояние: {{point.distance}}км</p>\n    <div class=\"table-wrapper\">\n      <i class=\"fa fa-rub table-icon\" aria-hidden=\"true\"></i>\n      <table>\n        <tbody>\n        <tr>\n          <td>Материал</td>\n          <td>{{point.materialPrice}} руб.</td>\n        </tr>\n        <tr>\n          <td>Доставка</td>\n          <td>{{point.delivery}} руб.</td>\n        </tr>\n        <tr>\n          <td>Итого</td>\n          <td>{{point.fullPrice}} руб.</td>\n        </tr>\n        </tbody>\n      </table>\n    </div>\n    <div class=\"order-form\">\n      <form>\n        <label for=\"name\">Имя</label>\n        <input id=\"name\" type=\"text\" name=\"name\" [(ngModel)]=\"name\">\n        <label for=\"phone\">Телефон</label>\n        <input id=\"phone\" type=\"tel\" name=\"phone\" [(ngModel)]=\"phone\">\n        <input type=\"submit\" value=\"Заказать\" (click)=\"onOrderClick(point)\">\n      </form>\n    </div>\n  </div>\n</div>\n"
 
 /***/ }),
 
@@ -354,7 +479,7 @@ var PointsComponent = (function () {
         this.eventsService = eventsService;
         this.backendService = backendService;
         this.searchForm = {};
-        this.points = [];
+        this.points = null;
         this.openedPoint = -1;
         this.name = '';
         this.phone = '';
@@ -362,15 +487,25 @@ var PointsComponent = (function () {
         eventsService.on('startLoadPoints', function () {
             _this.points = [];
             _this.pointsAreLoading = true;
+            _this.openedPoint = -1;
         });
         eventsService.on('endLoadPoints', function (res, form) {
             _this.pointsAreLoading = false;
             _this.points = res.data.items;
             _this.searchForm = form;
+            _this.addPricesToPoints();
         });
     }
+    PointsComponent.prototype.addPricesToPoints = function () {
+        var _this = this;
+        this.points.forEach(function (point) {
+            point.materialPrice = parseFloat(point.price) * parseFloat(_this.searchForm.count);
+            point.fullPrice = point.materialPrice + parseFloat(point.delivery);
+        });
+    };
     PointsComponent.prototype.onPointClick = function (index) {
         this.openedPoint = index;
+        this.eventsService.broadcast('openPoint', this.points[index], this.searchForm);
     };
     PointsComponent.prototype.onOrderClick = function (point) {
         var form = {
@@ -430,7 +565,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/searchForm/searchForm.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<form (ngSubmit)=\"onSubmit()\">\n  <fieldset>\n    <label for=\"complect\">Материал</label>\n    <select id=\"complect\"\n            [(ngModel)]=\"form.complect\"\n            name=\"complect\">\n      <option *ngFor=\"let com of complectsArray\" [value]=\"com.id\">{{ com.name }}</option>\n    </select>\n\n    <label for=\"address\">Адрес</label>\n    <input auto-complete\n         name=\"address\"\n         id=\"address\"\n         type=\"text\"\n         [(ngModel)]=\"form.address\"\n         [source]=\"getAddresses\"\n         display-property-name=\"name\"\n         value-property-name=\"\"\n         (valueChanged)=\"onAddressChoose($event)\"\n         [value-formatter]=\"addressValueFormatter\"\n         [list-formatter]=\"addressListFormatter\"/>\n\n    <label for=\"count\">Объем материала</label>\n    <div class=\"row\">\n      <div class=\"column column-67\">\n        <input type=\"number\"\n               id=\"count\"\n               [(ngModel)]=\"form.count\"\n               name=\"count\">\n      </div>\n      <div class=\"column column-33\">\n        <select id=\"unit\"\n                [(ngModel)]=\"form.unit\"\n                name=\"unit\">\n          <option value=\"т\">Тонн</option>\n          <option value=\"куб.м\">Куб.м.</option>\n        </select>\n      </div>\n    </div>\n\n    <input type=\"checkbox\" id=\"cardpay\">\n    <label class=\"label-inline\" for=\"cardpay\">Безналичный расчет (т.ч. НДС 18%)</label>\n    <div>\n      <input class=\"button-primary\" type=\"submit\" value=\"Найти предложения\">\n    </div>\n  </fieldset>\n</form>\n"
+module.exports = "<form (ngSubmit)=\"onSubmit()\">\n  <fieldset>\n    <label for=\"complect\">Материал</label>\n    <select id=\"complect\"\n            [(ngModel)]=\"form.complect\"\n            name=\"complect\">\n      <option *ngFor=\"let com of complectsArray\" [value]=\"com.id\">{{ com.name }}</option>\n    </select>\n\n    <label for=\"address\">Адрес</label>\n    <input auto-complete\n         name=\"address\"\n         id=\"address\"\n         type=\"text\"\n         [(ngModel)]=\"form.address\"\n         [source]=\"getAddresses\"\n         display-property-name=\"name\"\n         value-property-name=\"\"\n         (valueChanged)=\"onAddressChoose($event)\"\n         [value-formatter]=\"addressValueFormatter\"\n         [list-formatter]=\"addressListFormatter\"/>\n\n    <label for=\"count\">Объем материала</label>\n    <div class=\"row\">\n      <div class=\"column column-67\">\n        <input type=\"number\"\n               id=\"count\"\n               [(ngModel)]=\"form.count\"\n               name=\"count\"\n               required>\n      </div>\n      <div class=\"column column-33\">\n        <select id=\"unit\"\n                [(ngModel)]=\"form.unit\"\n                name=\"unit\">\n          <option value=\"т\">Тонн</option>\n          <option value=\"куб.м\">Куб.м.</option>\n        </select>\n      </div>\n    </div>\n\n    <input type=\"checkbox\" id=\"cardpay\">\n    <label class=\"label-inline\" for=\"cardpay\">Безналичный расчет (т.ч. НДС 18%)</label>\n    <div>\n      <input class=\"button-primary\" type=\"submit\" value=\"Найти предложения\">\n    </div>\n  </fieldset>\n</form>\n"
 
 /***/ }),
 
