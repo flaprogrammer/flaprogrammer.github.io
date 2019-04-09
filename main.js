b8c822915325737fcf662ccf187ee61d9404f07e
flaprogrammer <flaprogrammer@gmail.com>
2019-04-09 19:27:22 +0300

    234

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-window.onload = function () {
+(function () {
     var mobileMenu = document.getElementsByClassName('mobile-menu')[0];
     for (let i in mobileMenu.children) {
         mobileMenu.children[i].onclick = function () {
             mobileMenu.style.display = 'none';
         }
     }
-    var layout = this.document.getElementById('layout');
+    var layout = document.getElementById('layout');
     layout.onclick = closePortfolioModal;
     var modals = document.getElementsByClassName('modal');
     for (var i in modals) {
@@ -13,7 +13,11 @@ window.onload = function () {
             e.stopPropagation();
         }
     }
-}
+    layout.onscroll = function(e) {
+        e.preventDefault();
+    }
+})();
+
 
 function showMobileMenu() {
     var mobileMenu = document.getElementsByClassName('mobile-menu')[0];
@@ -25,14 +29,46 @@ function showPortfolioModal(modalId) {
     layout.classList.remove('hidden');
     var modal = document.getElementById(modalId);
     modal.classList.remove('hidden');
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('modal-open');
 }
 
-function closePortfolioModal(modalId) {
+function closePortfolioModal() {
     var layout = document.getElementById('layout');
     layout.classList.add('hidden');
     var modals = document.getElementsByClassName('modal');
-    for (var i in modals) {
+    for (var i = 0; i < modals.length; i++) {
+        modals[i].scrollTop = 0;
         modals[i].classList.add('hidden');
     }
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('modal-open');
+}
+
+function submitContactForm() {
+    let name = document.querySelector('#contact_form [name="name"]').value;
+    let email = document.querySelector('#contact_form [name="email"]').value;
+    let message = document.querySelector('#contact_form [name="message"]').value;
+
+    let submit = document.querySelector('#contact_form button');
+
+    submit.setAttribute('disabled', true);
+
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.post('https://www.enformed.io/uzkhhsdh/', {name, email, message})
+      .then(response => showSuccessContactModal(response))
+      .catch(error => console.log(error));
+
+    return false;
+}
+
+function showSuccessContactModal(response) {
+    let submit = document.querySelector('#contact_form button');
+
+    submit.setAttribute('disabled', false);
+    window.alert('Your message was successfully sent. I will contact you soon!');
+    document.querySelector('#contact_form [name="name"]').value = '';
+    document.querySelector('#contact_form [name="email"]').value = '';
+    document.querySelector('#contact_form [name="message"]').value = '';
 }
 
