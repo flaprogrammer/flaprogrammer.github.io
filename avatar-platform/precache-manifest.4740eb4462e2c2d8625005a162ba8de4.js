78c933c2783f52aa7865209af6fad50bebbb5995
flaprogrammer <flaprogrammer@gmail.com>
2019-04-30 23:12:09 +0300

    324

---
+++ avatar-platform/precache-manifest.4740eb4462e2c2d8625005a162ba8de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc22417dc941b96cb798456592ad95c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72783b0e7c3f9b36bb5c",
+    "url": "/static/css/2.6faac089.chunk.css"
+  },
+  {
+    "revision": "75a3e804af5c4ae1b17e",
+    "url": "/static/css/main.5ef07390.chunk.css"
+  },
+  {
+    "revision": "72783b0e7c3f9b36bb5c",
+    "url": "/static/js/2.e0cdfc40.chunk.js"
+  },
+  {
+    "revision": "75a3e804af5c4ae1b17e",
+    "url": "/static/js/main.a924a140.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
