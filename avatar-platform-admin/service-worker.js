301a7b0ba24f0df1690e5081ee7f88965c129764
flaprogrammer <flaprogrammer@gmail.com>
2019-05-15 21:10:20 +0300

    44

--- avatar-platform-admin/service-worker.js
+++ avatar-platform-admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/avatar-platform-admin/precache-manifest.62c267fab90ee81d0e54c422bc275d9a.js"
+  "/avatar-platform-admin/precache-manifest.b8dc0b0df9ff6457f8d4d1b6b1c62fdd.js"
 );
 
 self.addEventListener('message', (event) => {
