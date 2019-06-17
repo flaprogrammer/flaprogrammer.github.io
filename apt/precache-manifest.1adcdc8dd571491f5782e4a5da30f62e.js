36e6fd3736f172e22dd0d729b8f868da190dabd3
flaprogrammer <flaprogrammer@gmail.com>
2019-06-17 16:47:35 +0300

    324

---
+++ apt/precache-manifest.1adcdc8dd571491f5782e4a5da30f62e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff175afce2aad862cef3a1b95074d74",
+    "url": "/avatar-platform/index.html"
+  },
+  {
+    "revision": "bc1b69286f839ad3b9d3",
+    "url": "/avatar-platform/static/css/2.6faac089.chunk.css"
+  },
+  {
+    "revision": "8e9d7f41e78a775e638e",
+    "url": "/avatar-platform/static/css/main.f1809fd3.chunk.css"
+  },
+  {
+    "revision": "bc1b69286f839ad3b9d3",
+    "url": "/avatar-platform/static/js/2.b2c18982.chunk.js"
+  },
+  {
+    "revision": "8e9d7f41e78a775e638e",
+    "url": "/avatar-platform/static/js/main.89bb1eca.chunk.js"
+  },
+  {
+    "revision": "c745a33d24becba73b37",
+    "url": "/avatar-platform/static/js/runtime~main.b0f8d053.js"
+  },
+  {
+    "revision": "1fc4aeaa7d4f7b13b21febaab557fed0",
+    "url": "/avatar-platform/static/media/pipette.1fc4aeaa.svg"
+  }
+]);
